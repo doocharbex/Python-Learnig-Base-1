e4969074bc2f2555658ac8ee3ffbe5c7d692a489
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-09-03 18:31:37 +0430

    Add : Code Meeting 2

--- meeting_2.py
+++ meeting_2.py
@@ -1,3 +1,71 @@
 # Opretion
 # If Else Elif
+# Change Type
 # Project
+
+print('----------- Opretion ------------')
+
+print('-- " + " --')
+
+num_1 = 1 
+num_2 = 2
+
+sum = num_1 + num_2
+
+print(sum)
+
+print('---------------')
+
+print('-- " - " --')
+
+num_1 = 7 
+num_2 = 2
+
+min = num_1 - num_2
+
+print(min)
+
+
+print('---------------')
+
+print('-- " * " --')
+
+num_1 = 7 
+num_2 = 2
+
+zarb = num_1 * num_2
+
+print(zarb)
+
+print('---------------')
+
+print('-- " % " --')
+
+num_1 = 20 
+num_2 = 2
+
+taghsim = num_1 % num_2
+
+print(taghsim)
+
+
+
+print('----------- If Else Elif ------------')
+
+if 'aliReza' == 'aliReza' :
+    print('True')
+     
+else :
+    print('Flase')
+    
+print('----------- Change Type ------------')
+
+integer = int("2")
+print(integer)
+print(type(integer))
+
+string = ""
+str(string)
+print(string)
+print(type(string))
+
