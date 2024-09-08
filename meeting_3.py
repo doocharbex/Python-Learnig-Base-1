9db39130d42b0f8188d07f579bd07c9a54a4edbd
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-09-08 18:26:42 +0430

    Add : Code Meeting 3

---
+++ meeting_3.py
@@ -0,0 +1,45 @@
+# For -
+# list -
+# and & or
+# Score Alireza (10)
+# Score Erfan (0)
+
+print('-----------------------')
+
+print('List')
+
+listClass = ['Alireza', 'Erfan', 'Dayan']
+
+listClass.append('Erfan2')
+
+
+print(listClass)
+
+print(type(listClass))
+
+
+print('-----------------------')
+
+print('For')
+
+createInput =  int(input('Number Create Input : '))
+listCalsss = []
+
+for i in range(1,createInput+1) :
+    
+    doneCreateInput = input(f"Done Input {i} : ")
+    
+    
+    listCalsss.append(doneCreateInput)
+    
+print(listCalsss)
+
+# *
+# ** 
+# ***
+# ****
+# *****
+# ******
+# *******
+
+print('-----------------------')
