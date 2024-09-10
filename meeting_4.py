5643149d7e244c1aee243c25a2b70b9131299728
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-09-10 18:22:57 +0430

    Add : Code Meeting 4

---
+++ meeting_4.py
@@ -0,0 +1,43 @@
+# # While -
+# # Module --
+# # and & or & break
+
+print('-----------------------')
+
+print('While')
+
+inputUserRun = int(input("Number Run : "))
+
+i = 0 
+
+while inputUserRun == 2 :
+    i + 1
+    if i == 10 :
+        print("END While")
+        break
+    print(" Run While ! ")
+
+else :
+    print(" Fild Run While ")
+
+print('-----------------------')
+
+
+print('and & or')
+
+if 'aliReaz' == "aliReaz " or 'Erfan' == 'Erfan' :
+    print('bale hast')
+
+print('-----------------------')
+
+print('Module')
+
+import random
+
+resultRandom = random.randint(1,10)
+
+if resultRandom == 1 :
+    print('1')
+
+
+print('-----------------------')
